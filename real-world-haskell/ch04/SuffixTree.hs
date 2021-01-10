ffd9730edb450993e4c394b56589a0819e78d935
Rígille Scherrer Borges Menezes <rigillesbmenezes@protonmail.com>
2021-01-10 16:36:26 -0300

    compor funções de várias formas diferentes

--- real-world-haskell/ch04/SuffixTree.hs
+++ real-world-haskell/ch04/SuffixTree.hs
@@ -6,4 +6,13 @@ suffixes :: [a] -> [[a]]
 suffixes xs@(_:xs') = xs : suffixes xs'
 suffixes _ = []
 
-suffixes2 xs = init (tails xs)
+suffixes2 = init (tails xs)
+
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose f g x = f (g x)
+
+suffixes3 xs = compose init tails xs
+
+suffixes4 = compose init tails
+
+suffixes5 = init . tails
